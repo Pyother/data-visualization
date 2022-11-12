7b89ecd4442a49cdc74da4cc66f8927fbd37f22b
piotr <piotr.dominik.sobol@gmail.com>
2022-11-12 18:42:32 +0100

    Set size dependecies between Cube and Plane

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './index.css';
 
 function Box() {
   const mesh = useRef(null);
+  const edges = new THREE.EdgesGeometry();
   useFrame(() => (mesh.current.rotation.z += 0.002));
   return (
     <mesh ref={mesh}>
@@ -15,8 +16,8 @@ function Box() {
   );
 }
 
-const Cube = ({ position }) => (
-  <group position={position} >
+const Cube = ({ position, scale }) => (
+  <group position={position} scale={scale}>
     <Box/>
   </group>
 );
@@ -28,20 +29,27 @@ function Plane() {
     <mesh ref={mesh}>
       <planeBufferGeometry args={[5, 5, 5]}/>
       <meshLambertMaterial attach="material" color={"#0c181c"} side={THREE.DoubleSide}/>
-      
     </mesh>
   );
 }
 
+const Table = ({ scale }) => (
+  <group scale={scale}>
+    <Plane />
+  </group>
+);
+
 export default function App() {
   
+  let incubator_x = 1, incubator_y = 1, incubator_z = 1;
+
   return (
   <Canvas>
     <OrbitControls maxDistance={10} minDistance={2}/>
     <ambientLight intensity={0.5}/>
     <spotLight position={[10, 15, 10]} angle={0.3}/>
-    <Cube position={[0, 0, 0.5]}/>
-    <Plane/>
+    <Cube position={[0, 0, 0.5*incubator_z]} scale={[incubator_x, incubator_y, incubator_z]}/>
+    <Table scale={[incubator_x, incubator_y, 0]}/>
   </Canvas>
   
   );

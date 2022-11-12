f2daf516f88cb05d5fcda18d0ef7f81c0c9599d8
piotr <piotr.dominik.sobol@gmail.com>
2022-11-12 16:00:13 +0100

    Add Plane and set z-axis rotation of the scene

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import React from 'react';
-import { Canvas } from "react-three-fiber";
+import * as THREE from 'three'
+import React, { useRef } from 'react';
+import { Canvas, useFrame } from "react-three-fiber";
 import { OrbitControls } from "@react-three/drei";
 import './index.css';
 
 function Box() {
+  const mesh = useRef(null);
+  useFrame(() => (mesh.current.rotation.z += 0.002));
   return (
-    <mesh>
+    <mesh ref={mesh}>
       <boxBufferGeometry attach="geometry"/>
-      <meshLambertMaterial attach="material" color="hotpink"/>
+      <meshLambertMaterial attach="material" color="#1fd2ff" emissive="0a9ca0" wireframe={true}/>
     </mesh>    
   );
 }
 
-function Space() {
-  return (0);
+function Plane() {
+  const mesh = useRef(null);
+  useFrame(() => (mesh.current.rotation.z += 0.002));
+  return (
+    <mesh ref={mesh}>
+      <planeBufferGeometry args={[5, 5, 5]}/>
+      <meshLambertMaterial attach="material" color={"#0c181c"} />
+    </mesh>
+  );
 }
 
 export default function App() {
-  return <Canvas>
+  return (
+  <Canvas>
     <OrbitControls/>
     <ambientLight intensity={0.5}/>
+    <spotLight position={[10, 15, 10]} angle={0.3}/>
     <Box/>
-  </Canvas>;
+    <Plane/>
+
+  </Canvas>
+  );
 }
